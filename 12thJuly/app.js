23f730e570b71f8d9b5c7e40c99419fa24cd72c5
Sanket Parshuram Patil <53385614+sanket9006@users.noreply.github.com>
2021-07-15 15:53:47 +0530

    Backend for Complex Number

--- 12thJuly/app.js
+++ 12thJuly/app.js
@@ -1,14 +1,44 @@
-const calci = require('./operations')
-
-const express = require('express')
-const app = express()
-const port = 3000
-
-app.get('/', (req, res) => {
-    res.send('Hello World!' + `calci add ${calci.add(5, 5)}`)
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+// Create application/x-www-form-urlencoded parser  
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
+app.use(express.static('public'));
+app.get('/index.html', function (req, res) {
+    res.sendFile(__dirname + "/" + "index.html");
 })
+app.post('/process_post', urlencodedParser, function (req, res) {
+    // Prepare output in JSON format  
 
-app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
-    console.log(`calci add ${calci.add(5, 5)}`)
+    switch (req.body.operation) {
+        case "+":
+            var result = req.body.first_number + req.body.second_number;
+            break;
+        case "-":
+            var result = req.body.first_number - req.body.second_number;
+            break;
+        case "*":
+            var result = req.body.first_number * req.body.second_number;
+            break;
+        case "/":
+            var result = req.body.first_number / req.body.second_number;
+            break;
+        default:
+            var result = "Error: Invalid operation.";
+            break;
+    }
+
+    response = {
+        first_number: req.body.first_number,
+        second_number: req.body.second_number,
+        finaloutput: result
+    };
+
+    console.log(response);
+    res.end(JSON.stringify(response));
+})
+var server = app.listen(8000, function () {
+    var host = server.address().address
+    var port = server.address().port
+    console.log("Example app listening at http://%s:%s", host, port)
 })
